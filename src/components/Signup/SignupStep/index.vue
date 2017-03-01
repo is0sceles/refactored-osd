3be9411f85b5fd56833e8f261de91d8ef52a3d4f
elliottabirch <elliottabirch@gmail.com>
2017-02-28 23:51:15 -0800

    create personal info page of signup

---
+++ src/components/Signup/SignupStep/index.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <dating-prefs v-if="this.$route.params.step === 'dating'" :set-property='setProperty'></dating-prefs>
+  <image-upload v-else-if='this.$route.params.step === "image"' :set-property='setProperty'></image-upload>
+  <personal-info v-else-if='this.$route.params.step === "personal"' :set-property='setProperty'></personal-info>
+</div>
+</template>
+<script>
+import datingPrefs from './datingPrefs';
+import imageUpload from './imageUpload';
+import personalInfo from './personalInfo';
+
+export default {
+  computed: {
+  },
+  components: {
+    datingPrefs,
+    imageUpload,
+    personalInfo,
+  },
+  props: [
+    'setProperty',
+  ],
+};
+
+</script>
