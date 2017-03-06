334a7186638506247e34b19761affc42ee23d637
Eric Lau <elau002@gmail.com>
2017-03-06 14:23:07 -0800

    Shifted logic from computed to methods and calling on created

--- src/components/Calendar/week/index.vue
+++ src/components/Calendar/week/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div id='weekComp'>
-    <div v-for='days in daysOfTheWeek' class='weekClass'>
-      <day>{{days}} {{ calculateDate}}</day>
+    <div v-for='days in currentWeek' class='weekClass'>
+      <day>{{days}}</day>
     </div>
   </div>
 </template>
@@ -16,36 +16,39 @@
     },
     data () {
       return {
-        daysOfTheWeek: ['Sun','Mon','Tues','Wed','Thurs','Fri','Sat'],
         currentWeek: null,
       }
     }, 
-    computed: {
-      calculateDate: ($data)=> {
+    created() {
+      this.calculateDate(this.$data);
+    },
+    methods: {
+      calculateDate: (data)=> {
         let today = new Date();
         let past = today.getDay() - 0;
         let future = 6 - today.getDay();
         let thisWeek = [];
         for(var i = 0; i < past; i++) {
           let date = moment().subtract(past - i, 'days');
-          thisWeek = thisWeek.concat(date.format("dddd, MMMM Do "));
+          thisWeek = thisWeek.concat(date.format("ddd, Do"));
         }
-          thisWeek = thisWeek.concat(moment().format("dddd, MMMM Do "));
+          thisWeek = thisWeek.concat(moment().format("ddd, Do"));
         for(var i = 1 ; i <= future; i++ ) {
           let date = moment().add(i, 'days');
-          thisWeek = thisWeek.concat(date.format("dddd, MMMM Do "));
+          thisWeek = thisWeek.concat(date.format("ddd, Do"));
         }
-        console.log(thisWeek);
-        return $data.daysOfTheWeek[today.getDay()];
+        data.currentWeek = thisWeek;
+        console.log(data.currentWeek);
+        return data.currentWeek;
       }
-    },
+    }
   }
 </script>
 
 <style>
   div#weekComp {
     width: 100%;
-    padding-top: 8%;
+    padding-top: 5%;
     margin-left: 15%;
   }
   div.weekClass {
