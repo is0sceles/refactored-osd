e2ea0d83893182d392937a4b708f921ecf1cd363
Eric Lau <elau002@gmail.com>
2017-03-06 11:25:30 -0800

    Added month component to calendar

--- src/components/Calendar/index.vue
+++ src/components/Calendar/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      Calendar
+      <h3> {{month[today.getMonth()]}} </h3>
       <week></week>
   </div>
 </template>
@@ -8,18 +8,26 @@
 
 <script>
   import week from './week';
+  import Calendar from 'vue-calendar-mobile';
+
   export default {
     components: {
+      Calendar,
       week,
     },
     data () {
       return {
-
+        month: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        today: new Date(),
       }
     },
     methods: {
 
     },
+    computed: {
+      now: ()=> {
+      }
+    }
   }
 
 </script>
