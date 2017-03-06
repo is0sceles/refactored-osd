2adab36b554314f76940804bc2cd949dc4216763
Eric Lau <elau002@gmail.com>
2017-03-06 14:24:07 -0800

    Updated router to include calendar

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,9 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import LandingPage from '../components/LandingPage';
 import NavBar from '../components/NavBar';
-<<<<<<< HEAD
-import SignupStep from '../components/Signup/SignupStep'
-import Signup from '../components/Signup'
-=======
+import SignupStep from '../components/Signup/SignupStep';
+import Signup from '../components/Signup';
 import Calendar from '../components/Calendar';
->>>>>>> Updated router to have basic path to calendar
 
 Vue.use(Router);
 
@@ -23,7 +20,6 @@ export default new Router({
       name: 'user',
       component: NavBar,
       children: [
-<<<<<<< HEAD
         {
           path: 'signup',
           name: 'SignupMain',
@@ -35,24 +31,11 @@ export default new Router({
             }
           ]
         },
-=======
-        // {
-        //   path: 'signup',
-        //   name: 'SignupMain',
-        //   component: SignUp,
-        //   children: [
-        //     {
-        //       path: ':id',
-        //       component: SignupStep
-        //     }
-        //   ]
-        // },
         {
           path: 'calendar',
           name: 'calendar',
           component: Calendar,
-        }
->>>>>>> Updated user path to go to calendar
+        },
       ]
     },
   ],
