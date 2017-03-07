01a23b3ab68a42e66e6d3b84c18dd77e56a74157
Wasiff <wasiff@gmail.com>
2017-03-07 14:12:09 -0800

    create imageUploader template

---
+++ src/components/imageUploader/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="!image">
+    <h3> Select an image </h3>
+    <input type="file" onChange="onchange">
+  </div>
+ <div v-else>
+   <img :src="image" />
+    <button @click="removeImage">Remove image</button>
+ </div>
+
+</template>
+
+<script>
+ export default {
+   data: {},
+  methods: {
+      onchange(e) {
+        console.log('inside onchange', e)
+      }
+  }
+ } 
+</script>
+
+<style>
+</style>
