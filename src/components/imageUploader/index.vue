f00d55b41520746b320c292cfd2b705b1357709c
Wasiff <wasiff@gmail.com>
2017-03-07 16:54:52 -0800

    edit imageLoader component template and logic

--- src/components/imageUploader/index.vue
+++ src/components/imageUploader/index.vue
@@ -1,24 +1,42 @@
 <template>
-  <div v-if="!image">
-    <h3> Select an image </h3>
-    <input type="file" onChange="onchange">
+    <div v-if="!image">
+        <h3> {{ header }} </h3>
+        <input type="file" @change="onchange">
+    </div>
+  <div v-else>
+        <img :src="image" />
+        <button @click="removeImage">Remove image</button>
   </div>
- <div v-else>
-   <img :src="image" />
-    <button @click="removeImage">Remove image</button>
- </div>
-
 </template>
 
 <script>
- export default {
-   data: {},
+  export default {
+    data: {
+    image: '',
+    header: 'Select an image'
+  },
   methods: {
-      onchange(e) {
-        console.log('inside onchange', e)
-      }
+    onchange(e) {
+      let files = e.target.files || e.dataTransfer.files
+      if (!files.length)
+        return;
+      this.createImage(files[0])
+    },
+    createImage(file) {
+      let image = new Image()
+      let reader = new FileReader()
+      let model = this
+      console.log(model)
+      reader.onload = (e) => {
+        model.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    },
+    removeImage: function (e) {
+      this.image = '';
+    }
   }
- } 
+  } 
 </script>
 
 <style>
